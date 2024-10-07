3b2a7dc320a7a82437473e20e76c773a4829bc07
Rafael Nunes <rafaelnunes@iMac-Pro-de-Rafael.local>
2024-10-07 12:16:56 -0300

    Populando lista a partir do mock

--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,10 +1,30 @@
 import React from "react"
-import { Text } from "react-native"
+import { View, FlatList, Text } from "react-native"
+import users from "../data/users"
+import { ListItem } from '@rneui/themed'
 
 export default props => {
+    console.warn(Object.keys(props))
+
+    function getUserItem({ item: user }) {
+        return (
+            <ListItem
+                leftAvatar={{source: {uri: user.avatarUrl}}}
+                key={user.id}
+                title={user.name}
+                subtitle={user.email}
+                bottomDivider
+            />
+        )
+    }
+
     return (
-        <Text>
-            UserList
-        </Text>
+        <View>
+            <FlatList
+                keyExtractor={user => user.id.toString()}
+                data={users}
+                renderItem={getUserItem}
+            />
+        </View>
     )
 }

02c1ca2a918239899d7b42481eaedf094006f640
Rafael Nunes <rafaelnunes@iMac-Pro-de-Rafael.local>
2024-10-07 13:11:58 -0300

    Atualizando de acordo com a documentação

--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,20 +1,23 @@
 import React from "react"
-import { View, FlatList, Text } from "react-native"
+import { View, FlatList } from "react-native"
 import users from "../data/users"
-import { ListItem } from '@rneui/themed'
+import { Avatar, ListItem } from '@rneui/themed'
 
 export default props => {
     console.warn(Object.keys(props))
 
     function getUserItem({ item: user }) {
         return (
-            <ListItem
-                leftAvatar={{source: {uri: user.avatarUrl}}}
-                key={user.id}
-                title={user.name}
-                subtitle={user.email}
-                bottomDivider
-            />
+            <ListItem botto>
+                <Avatar
+                    rounded
+                    source={{ uri: user.avatarUrl }}
+                />
+                <ListItem.Content>
+                    <ListItem.Title>{user.name}</ListItem.Title>
+                    <ListItem.Subtitle>{user.email}</ListItem.Subtitle>
+                </ListItem.Content>
+            </ListItem>
         )
     }
 

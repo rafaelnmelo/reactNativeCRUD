863ab32da4fbf859a3fe160bbd30fc65737c9f60
Rafael Nunes <rafaelnunes@iMac-Pro-de-Rafael.local>
2024-10-10 11:04:55 -0300

    Layout do formulário

--- src/views/UserForm.js
+++ src/views/UserForm.js
@@ -1,13 +1,55 @@
 import React, { useState } from "react"
-import { Text } from "react-native"
+import { Text, TextInput, View, StyleSheet, Button } from "react-native"
 
-export default ({route, navigation}) => {
+export default ({ route, navigation }) => {
     console.warn(Object.keys(route))
 
     const [user, setUser] = useState(route.params ? route.params : {})
     return (
-        <Text>
-            {user.id}
-        </Text>
+        <View style={style.form}>
+            <Text>Nome</Text>
+            <TextInput
+                style={style.input}
+                onChangeText={name => setUser({ ...user, name })}
+                placeholder="Informe o Nome"
+                value={user.name}
+            />
+            <Text>Email</Text>
+            <TextInput
+                style={style.input}
+                onChangeText={email => setUser({ ...user, email })}
+                placeholder="Informe o E-mail"
+                value={user.email}
+            />
+            <Text>URL do Avatar</Text>
+            <TextInput
+                style={style.input}
+                onChangeText={avatarUrl => setUser({ ...user, avatarUrl })}
+                placeholder="Informe a URL do Avatar"
+                value={user.avatarUrl}
+            />
+            <Button
+                title="Salvar"
+                onPress={() => {
+                    dispatch({
+                        type: user.id ? 'updateUser' : 'createUser',
+                        payload: user,
+                    })
+                    navigation.goBack()
+                }}
+            />
+        </View>
     )
 }
+
+const style = StyleSheet.create({
+    form: {
+        padding: 12
+    },
+    input: {
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        marginBottom: 15,
+    }
+})

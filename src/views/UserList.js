ce3cd1b4ed8dd2cf5514515a69a38de7b495480a
Rafael Nunes <rafaelnunes@iMac-Pro-de-Rafael.local>
2024-10-10 10:34:11 -0300

    Criando botões de edição e exclusão

--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,14 +1,28 @@
 import React from "react"
-import { View, FlatList } from "react-native"
+import { View, FlatList, Alert } from "react-native"
 import users from "../data/users"
 import { Avatar, ListItem } from '@rneui/themed'
 
 export default props => {
     console.warn(Object.keys(props))
 
+    function confirmUserDeletion(user) {
+        Alert.alert('Excluir Usuário', 'Deseja excluir o usuário?', [
+            {
+                text: 'Sim',
+                onPress() {
+                    console.warn("Fake delete user " + user.id)
+                }
+            },
+            {
+                text: 'Não'
+            }
+        ])
+    }
+
     function getUserItem({ item: user }) {
         return (
-            <ListItem botto>
+            <ListItem bottomDivider >
                 <Avatar
                     rounded
                     source={{ uri: user.avatarUrl }}
@@ -17,6 +31,16 @@ export default props => {
                     <ListItem.Title>{user.name}</ListItem.Title>
                     <ListItem.Subtitle>{user.email}</ListItem.Subtitle>
                 </ListItem.Content>
+                <ListItem.Chevron
+                    onPress={() => props.navigation.navigate('UserForm', user)}
+                    iconProps={{ name: "edit" }}
+                    iconStyle={{ fontSize: 25, color: "orange" }}
+                />
+                <ListItem.Chevron
+                    onPress={() => confirmUserDeletion(user)}
+                    iconProps={{ name: 'delete' }}
+                    iconStyle={{ fontSize: 25, color: "red" }}
+                />
             </ListItem>
         )
     }
